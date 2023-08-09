c9a69b797a5118c0b836aed0844dd301dfc01f38
shs2783 <shs2783_@naver.com>
2023-08-10 01:56:38 +0900

    feat: add text parsing

--- back/main.py
+++ back/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
 import openai
-from fastapi import UploadFile
+
+from fastapi import FastAPI, UploadFile
 from fastapi.exceptions import HTTPException
+
+from text_parsing import parse_menu, parse_answer
 
 openai.api_key = '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'
 
@@ -14,7 +16,7 @@ def save_audio_file(contents, audio_file_name="audio.mp3"):
         audio_file = open(audio_file_name, "rb")
         return audio_file
 
-@app.post("/api/stt")
+@app.post("/api/stt/menu")
 async def speech_to_text(audio_file: UploadFile):
     contents = await audio_file.read()
     audio_file = save_audio_file(contents)
@@ -27,4 +29,21 @@ async def speech_to_text(audio_file: UploadFile):
     if not transcript:
         return HTTPException(status_code=400, detail="Failed to decode audio")
 
-    return transcript
+    order = parse_menu(transcript)
+    return order
+
+@app.post("/api/stt/answer")
+async def speech_to_text(audio_file: UploadFile):
+    contents = await audio_file.read()
+    audio_file = save_audio_file(contents)
+
+    try:
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    except:
+        return HTTPException(status_code=400, detail="Failed to load audio file")
+
+    if not transcript:
+        return HTTPException(status_code=400, detail="Failed to decode audio")
+
+    answer = parse_answer(transcript)
+    return answer
---
+++ back/text_parsing.py
@@ -0,0 +1,43 @@
+from functools import partial
+from db import menus, sets, nums, take_outs, answers
+
+def parse_option(option, order, text, word, value):
+    if word in text:
+        order[option] = value
+
+def parse_iterator(parse_func, order, text, **data):
+    for key, values in data.items():
+        for val in values:
+            parse_func(order, text, val, key)
+
+_parse_menu = partial(parse_option, 'menu')
+_parse_num = partial(parse_option, 'amount')
+_parse_set = partial(parse_option, 'set')
+_parse_take_out = partial(parse_option, 'take_out')
+_parse_answer = partial(parse_option, 'answer')
+
+def parse_menu(text):
+    order = {
+        'menu': None,  # 메뉴명
+        'set': '단품',  # 단품 / 세트
+        'amount': 1,  # 수량
+        'take_out': '매장',  # 포장 / 매장
+    }
+
+    parse_iterator(_parse_menu, order, text, **menus)
+    parse_iterator(_parse_set, order, text, **sets)
+    parse_iterator(_parse_num, order, text, **nums)
+    parse_iterator(_parse_take_out, order, text, **take_outs)
+
+    return order
+
+def parse_answer(text):
+    answer = {'answer': None}
+    parse_iterator(_parse_answer, answer, text, **answers)
+
+    return answer
+
+
+if __name__ == '__main__':
+    order = parse_menu(text="빙맥 세투 두게 포장 부탁해요")
+    print(order)
