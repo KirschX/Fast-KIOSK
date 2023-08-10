f8c6ab15698c37eb9d8fdb4b936986d00d91fb8b
shs2783 <shs2783_@naver.com>
2023-08-11 02:00:43 +0900

    feat: add answer text parsing in the endpoint

--- back/main.py
+++ back/main.py
@@ -3,9 +3,9 @@ import openai
 
 from fastapi import FastAPI, UploadFile
 from fastapi.exceptions import HTTPException
+from pydantic import BaseModel
 
 from text_parsing import parse_menu, parse_answer
-
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,6 +13,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
 
+class Text(BaseModel):
+    text: str
+    
 def save_audio_file(contents, audio_file_name="audio.mp3"):
     with open(audio_file_name, "wb") as f:
         f.write(contents)
@@ -62,6 +65,10 @@ async def speech_to_text_answer(audio_file: UploadFile):
 
     return parse_answer(transcript)
 
-@app.post("/api/text-parsing")
-async def parse_text(text: str):
-    return parse_menu(text)
+@app.post("/api/text/menu")
+async def parse_text_menu(text: Text):
+    return parse_menu(text.text)
+
+@app.post("/api/text/answer")
+async def parse_text_answer(text: Text):
+    return parse_answer(text.text)
