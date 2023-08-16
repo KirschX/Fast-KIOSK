f5e7a8f1fedcb62d3187822f280f74cd61e1a0bc
KirschX <mk44879@gmail.com>
2023-08-17 01:46:10 +0900

    feat: added useVoiceGuidance hook

---
+++ front/src/hooks/useVoiceGuidance.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+const useVoiceGuidance = () => {
+  const [voiceStarted, setVoiceStarted] = useState(false);
+  const [utterance, setUtterance] = useState<SpeechSynthesisUtterance | null>(
+    null
+  );
+
+  const startVoiceGuidance = (input: string, onEndCallback?: () => void) => {
+    const lang = "ko-KR";
+    const script = input;
+    const newUtterance = new SpeechSynthesisUtterance(script);
+    newUtterance.lang = lang;
+    newUtterance.rate = 2;
+
+    if (onEndCallback) {
+      newUtterance.onend = (event) => {
+        onEndCallback();
+        setVoiceStarted(false);
+      };
+    } else {
+      newUtterance.onend = () => setVoiceStarted(false);
+    }
+
+    window.speechSynthesis.speak(newUtterance);
+    setUtterance(newUtterance);
+    setVoiceStarted(true);
+  };
+
+  const stopVoiceGuidance = () => {
+    if (utterance) {
+      window.speechSynthesis.cancel();
+      setVoiceStarted(false);
+    }
+  };
+
+  return { voiceStarted, startVoiceGuidance, stopVoiceGuidance };
+};
+
+export default useVoiceGuidance;
