dc5b0b8cbde3a85cb572f3ed1fe7ba953c8624d4
KirschX <mk44879@gmail.com>
2023-08-24 17:33:57 +0900

    feat: added helper funtions

---
+++ front/src/helper/audioConverter.ts
@@ -0,0 +1,14 @@
+// import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
+// const ffmpeg = createFFmpeg({ log: true });
+export default function convertWavToMp3() {
+  // await ffmpeg.load();
+  // // Write the wav file to ffmpeg's in-memory filesystem
+  // ffmpeg.FS("writeFile", "input.wav", await fetchFile(wavBlob));
+  // // Run the ffmpeg command to convert
+  // await ffmpeg.run("-i", "input.wav", "output.mp3");
+  // // Read the result from the in-memory filesystem
+  // const mp3Data = ffmpeg.FS("readFile", "output.mp3");
+  // return new Blob([mp3Data.buffer], { type: "audio/mp3" });
+  return;
+}
