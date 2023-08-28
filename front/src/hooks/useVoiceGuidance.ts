7ee719b252d9994e14c9fcda0bce8d97d9179620
KirschX <mk44879@gmail.com>
2023-08-28 18:22:20 +0900

    fix: voiceGuidance speed changed

--- front/src/hooks/useVoiceGuidance.ts
+++ front/src/hooks/useVoiceGuidance.ts
@@ -11,7 +11,7 @@ const useVoiceGuidance = () => {
     const script = input;
     const newUtterance = new SpeechSynthesisUtterance(script);
     newUtterance.lang = lang;
-    newUtterance.rate = 2;
+    newUtterance.rate = 1;
 
     if (onEndCallback) {
       newUtterance.onend = (event) => {
